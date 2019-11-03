63bdbb2724d76a24c7a03e8753396a8f36dae3c2
Osama Soliman <eng.microsmsm@gmail.com>
2019-11-03 13:18:40 +0200

    Benchmarking json deserialization using javascript

---
+++ JSON-Parse-JS/app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const JSONstring = fs.readFileSync('drugs.json').toString()
+//benchmark scoope
+const BenchmarkMS = (name, block) => {
+    const start = +new Date();
+    block();
+    const end = +new Date();
+    console.log(name, 'took', end - start);
+    return end - start
+};
+//one time experment
+BenchmarkMS('json parsing', () => {
+    const json = JSON.parse(JSONstring)
+})
+
+function benchOnScale(name, times, block) {
+    let totalTime = 0;
+    for (let i = 0; i < times; i++) {
+        totalTime = totalTime + BenchmarkMS(name, () => {
+            block()
+        })
+
+    }
+    let avarage = totalTime / times
+    console.log('Total of invoking', name, times, 'times', 'took', avarage);
+}
+
+//100 times experment
+benchOnScale('JsonParsing', 100, () => {
+    const json = JSON.parse(JSONstring)
+})
+
+
+
