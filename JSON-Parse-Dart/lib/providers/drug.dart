cba8eeaa3f1add1dd0845504a98320e6448db5cc
Osama Soliman <eng.microsmsm@gmail.com>
2019-11-25 05:20:50 +0200

    Update drug.dart

--- JSON-Parse-Dart/lib/providers/drug.dart
+++ JSON-Parse-Dart/lib/providers/drug.dart
@@ -5,19 +5,37 @@ import 'dart:convert';
 Future<List<Drug>>fetchDrugs() async{
   var response = await http.get('https://dawaey.com/api/v3/eg/drugs.json');
   List<Drug> drugs;
-  print("hi");
-  BenchmarkMS((){
-    var l = json.decode(response.body)['drugs'] as List;
-    drugs = l.map((model)=> Drug.fromJson(model)).toList();
+  var drugsJSON;
+
+  //one time benchmark
+  BenchmarkMS("JSON parse",(){
+    drugsJSON = json.decode(response.body)['drugs'] as List;
   });
+
+  //100 times benchmark
+  BenchmarkOnScale("JSON parse",100,(){
+    drugsJSON = json.decode(response.body)['drugs'] as List;
+  });
+
+  drugs = drugsJSON.map((model)=> Drug.fromJson(model)).toList();
   return drugs;
 }
 
-void BenchmarkMS(block){
+int BenchmarkMS(name, block){
   var start = new DateTime.now().millisecondsSinceEpoch;
   block();
   var end = new DateTime.now().millisecondsSinceEpoch;
-  print("****************************");
   print("Benchmark took " + (end-start).toString());
-  print("****************************");
+  return end - start;
+}
+
+void BenchmarkOnScale(String name,int times,block){
+  var totalTime = 0;
+  for (var i = 0; i < times; i++) {
+    totalTime = totalTime + BenchmarkMS(name, (){
+      block();
+    });
+  }
+  var avarage = totalTime / times;
+  print('Total of invoking '+ name + " "+ times.toString()+ ' times '+ 'took '+ avarage.toString());
 }
